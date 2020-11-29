ddcd6ae6dbf3a4993eb2d1074fd67fd081fb8274
Gabriel Dantas <gabrielmd123@gmail.com>
2020-11-29 20:32:00 -0300

    HomeCard component, nuxt.config crated

---
+++ components/HomeCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="card">
+      <img :src="home.images[0]" alt="">
+      <h3>{{home.title}}</h3>
+      <span>{{home.location.adress}}</span>
+      <span>{{home.location.city}}</span>
+      <span>{{home.location.state}}</span>
+      <p>{{home.guests}} guests</p>
+      <p>{{home.price}} / night</p>
+  </article>
+</template>
+
+<script>
+export default {
+    props: {
+        home: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style>
+img {
+    max-width: 200px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>NuxtBnB</div>
+    <div>
+        <h1>NuxtBnB</h1>
+        <ul>
+            <li v-for="(home, k) in homes" :key="k" >
+                <HomeCard :home='home'/>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-export default {
+import homes from '@/data/homes'
+import HomeCard from '@/components/HomeCard'
 
+export default {
+    components: {
+        HomeCard
+    },
+    data(){
+        return {
+            homes: homes.slice(0,3)
+        }
+    }
 }
 </script>
 
 <style>
+* {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+}
 
+ul {
+    list-style: none;
+}
 </style>
