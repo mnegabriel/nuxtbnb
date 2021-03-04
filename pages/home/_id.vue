49d24e9592e181c1280e76859aa83c8f73ed76cc
Gabriel Dantas <gabriel.dantas@catskillet.com>
2021-03-04 17:39:00 -0300

    refactor: agrupando chamadas api para acontecer ao mesmo tempo

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -70,36 +70,24 @@ export default {
         }
     },
     async asyncData({ params, $dataApi, error }) {
-        const homeResponse = await $dataApi.getHome(params.id)
-        if(!homeResponse.ok){ 
+        const responses = await Promise.all([
+            $dataApi.getHome(params.id),
+            $dataApi.getReviewsByHomeId(params.id),
+            $dataApi.getUserByHomeId(params.id)
+        ])
+
+        const badResponse = responses.find(response => !response.ok)
+        if(badResponse) {
             return error({ 
                 statusCode: homeResponse.status,
                 message: homeResponse.statusText    
-            })        
+            })
         }
-
-        const reviewResponse = await $dataApi.getReviewsByHomeId(params.id)
-        if(!reviewResponse.ok){ 
-            return error({ 
-                statusCode: reviewResponse.status,
-                message: reviewResponse.statusText    
-            })        
-        }
-
-        const userResponse = await $dataApi.getUserByHomeId(params.id)
-        if(!userResponse.ok){ 
-            return error({ 
-                statusCode: userResponse.status,
-                message: userResponse.statusText    
-            })        
-        }
-
-        console.log(userResponse.json.hits)
 
         return {
-            home: homeResponse.json,
-            reviews: reviewResponse.json.hits,
-            user: userResponse.json.hits[0],
+            home: responses[0].json,
+            reviews: responses[1].json.hits,
+            user: responses[2].json.hits[0],
         }
     },
     mounted() {
